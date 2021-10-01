902b7607ce294139db77bf03132ea85e934dd139
MarcoHansen7 <s194302@student.dtu.dk>
2021-10-01 13:14:26 +0200

    Rafflebærgertest 100 kast

--- src/spil/Raflebæger_test.java
+++ src/spil/Raflebæger_test.java
@@ -1,4 +1,45 @@
-package spil;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+    public class Raflebæger_test {
 
-public class Raflebæger_test {
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int dice1, dice2, sum1, sumTotal;
+            int[] opdel = new int[1000];
+            for (int i=0; i <1000 ;i++ ) {
+                Random forste = new Random();
+                dice1 = forste.nextInt(6);
+                dice1++;
+                Random anden = new Random();
+                dice2 = anden.nextInt(6);
+                dice2++;
+                sum1 = (dice1+dice2);
+                opdel [i]=sum1;
+                System.out.println("du slog " + dice1 + " og " + dice2 + " din sum er: " + sum1);
+            }
+
+            System.out.println(Arrays.toString(opdel));
+
+            System.out.println("Indtast det tal du vil se hyppigheden på");
+            int x = sc.nextInt();
+            int gange=0;
+
+
+            for(int i=0;i<1000;i++) {           //vi laver et for loop med en if-statement for som
+                if (x==opdel[i])
+                {
+                    gange++;
+                }
+            }
+
+            System.out.println(x+" optrådte "+gange+" gange ");
+
+
+        }
+
+    }
+
+
+
+
